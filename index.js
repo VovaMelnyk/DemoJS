c6345c910878f7d8bbbdf5a6d23d5c1ae648d12d
Vova Melnyk <hagmen4339@gmail.com>
2022-04-11 18:55:20 +0300

    add lesson 1

---
+++ index.js
@@ -0,0 +1,128 @@
+// # Модуль 1. Занятя 1.
+
+// ## Example 1 - Базові математичні операції
+
+// Виведи на екран загальну кількість яблук і винограду. Різницю я та винограду
+
+// ```js
+// const apples = 47;
+// const grapes = 135;
+// const total = ;
+// console.log(total)
+// const diff = ;
+// console.log(diff)
+// ```
+
+// ## Example 3 - Пріоритет операторів
+
+// Розбери пріоритет операторів
+// `result`.
+
+// ```js
+// const result = 108 + 223 - 2 * 5;
+// console.log(result);
+// ```
+
+// ## Example 4 - Клас Math
+
+// Напиши код який виводить в консоль округлення вверх / вниз.Використовуй методи`Math.floor()`,
+// `Math.ceil()` і`Math.round()`.
+
+// ```js
+// const value = 27.5;
+// ```
+
+// ## Example 5 - Шаблонні рядки
+
+// Склади фразу`A has B bots in stock`, де A, B - це змінні
+
+// ```js
+// const companyName = 'Cyberdyne Systems';
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = ``;
+// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+// ```
+
+// ## Example 6 - Оператори порівняння і приведення типів
+
+// Яким буде результат виконання коду?
+
+// ```js
+// console.log(5 > 4);
+
+// console.log(10 >= '7');
+
+// console.log('2' > '12');
+
+// console.log('2' < '12');
+
+// console.log('4' == 4);
+
+// console.log('6' === 6);
+
+// console.log('false' === false);
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+// console.log('0' == false);
+
+// console.log('0' === false);
+
+// console.log('Papaya' < 'papaya');
+
+// console.log('Papaya' === 'papaya');
+
+// console.log(undefined == null);
+
+// console.log(undefined === null);
+// ```
+
+// ## Example 7 - Опертор % та методы рядків
+
+// Напиши код який переводить значення`totalMinutes`(кількість хвилин) в формат
+//  `HH:MM`
+
+// - 70 покаже 01:10
+// - 450 покаже 07:30
+// - 1441 покаже 24:01
+
+// ```js
+// const totalMinutes = 70;
+
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+// ```
+// ## Example 8 - Логічні оператори
+
+// Яким буде результат виразу
+
+// ```js
+// console.log(true && 3);
+
+// console.log(false && 3);
+
+// console.log(true && 4 && 'kiwi');
+
+// console.log(true && 0 && 'kiwi');
+
+// console.log(true || 3);
+
+// console.log(true || 3 || 4);
+
+// console.log(true || false || 7);
+
+// console.log(null || 2 || undefined);
+
+// console.log((1 && null && 2) > 0);
+
+// console.log(null || (2 && 3) || 4);
+// ```
