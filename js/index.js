0ba482d9b16e13c1c78e8e29d5fbf328b793b01e
Vova Melnyk <hagmen4339@gmail.com>
2022-04-13 21:38:03 +0300

    finish lesson 2

--- js/index.js
+++ js/index.js
@@ -173,16 +173,25 @@
 // ```
 // ## Example 8 - Логічні оператори
 
-console.log("first");
-
 // Яким буде результат виразу
 
+// && - name === 'user' && login === 'admin' // всі true, зупиняється тоді коли будь - яка дасть false
+// || - name === 'user' || login === 'admin' // перший true який знайдемо , або останій flase в ланцюжку
+
 // ```js
-// console.log(true && 3);
+// console.log(true && 3); // 3
 
 // console.log(false && 3);
 
-// console.log(true && 4 && 'kiwi');
+// console.log(true && 4 && "kiwi"); // kiwi
+
+// 0
+// ''
+// false
+// null
+// undefined
+// NaN
+// -0
 
 // console.log(true && 0 && 'kiwi');
 
@@ -190,13 +199,15 @@ console.log("first");
 
 // console.log(true || 3 || 4);
 
-// console.log(true || false || 7);
+// console.log(false || true || 7);
 
 // console.log(null || 2 || undefined);
 
-// console.log((1 && null && 2) > 0);
+// console.log(1 && null && 2);
 
-// console.log(null || (2 && 3) || 4);
+// console.log((1 && null && 2) > 0); // null > 0
+
+// console.log(null || (2 && 3) || 4); // 2 && 3 => null || 3 || 4 => 3
 // ```
 
 // Number.parseInt()
@@ -207,9 +218,59 @@ console.log("first");
 
 // ## Example 1 - Ввід користувача та розгалуження
 
+// if () {
+
+// } else {
+
+// }
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// if () {
+
+// } else if () {
+
+// } else if () {
+
+// } else {
+
+// }
+
+// switch (key) {
+//     case value:
+
+//         break;
+//     case value:
+
+//         break;
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
 // Використовуючи конструкцію if...else та prompt напишіть код що буде питатися в користувача
 // "Яка офіційна назва JavaScript".Якщо користувач вводить `ECMAScript` то показувати alert з повідомленням`"Вірно!"`
 // в іншому випадку - "Не знаєте? ECMAScript"
+
+// const userInput = prompt("Яка офіційна назва JavaScript");
+
+// confirm("Are you ready ?");
+
+// if (userInput === "ECMAScript") {
+//   alert("Вірно");
+// } else {
+//   alert("Не знаєте? ECMAScript");
+// }
 
 // ## Example 2 - Показувати час(if...else)
 
@@ -219,7 +280,12 @@ console.log("first");
 // ```js
 // const hours = 14;
 // const minutes = 26;
-// let timestring;
+// let timeString;
+// if (minutes > 0) {
+//   timeString = `${hours} год ${minutes} хв`;
+// } else {
+//   timeString = `${hours} год`;
+// }
 // ```
 
 // ## Example 3 - Розгалуження
@@ -228,7 +294,34 @@ console.log("first");
 // Якщо користувач ввів 0 то рядок "Це нуль". Якщо користувач ввів відємне число виводити на екран рядок "Це відємне число"
 
 // ```js
-// const userInput = prompt('Введите число');
+// const userInput = prompt("Введіть число");
+// // > < >= <= === ==
+// if (userInput > 0) {
+//   console.log("Це додатнє число");
+// } else if (userInput < 0) {
+//   console.log("Це відємне число");
+// } else if (Number(userInput) === 0) {
+//   // "0" === 0
+//   console.log("Це нуль");
+// } else {
+//   console.log("Користувач ввів не число");
+// }
+
+// switch (true) {
+//   case userInput > 0:
+//     console.log("Це додатнє число");
+//     break;
+//   case userInput < 0:
+//     console.log("Це відємне число");
+//     break;
+//   case Number(userInput) === 0:
+//     console.log("Це нуль");
+//     break;
+//   default:
+//     console.log("Користувач ввів не число");
+//     break;
+// }
+
 // ```
 
 // ## Example 4 - if...else і логічні оператори
@@ -242,7 +335,16 @@ console.log("first");
 // - більше `24` , виводимо рядок `"Overdue"`
 
 // ```js
-// const hours = 10;
+// const hours = 0;
+
+// if (hours < 17) {
+//   alert(`${hours} is pending`);
+// } else if (hours >= 17 && hours <= 24) {
+//   alert("Expires");
+// } else {
+//   alert("Overdue");
+// }
+
 // ```
 
 // ## Example 5 - Дедлайн здачі проекта(if...else)
@@ -255,12 +357,73 @@ console.log("first");
 // - Якщо до дедлайну 3+ днів - виведи рядок `"Ще не скоро )"`
 
 // ```js
-// const daysUntilDeadline = 5;
+const daysUntilDeadline = 2;
+
+// if (daysUntilDeadline === 0) {
+//   console.log("Сьогодні");
+// } else if (daysUntilDeadline === 1) {
+//   console.log("Завтра");
+// } else if (daysUntilDeadline === 2) {
+//   console.log("Післязавтра");
+// } else {
+//   console.log("Ще не скоро )");
+// }
 // ```
 
 // ## Example 6 - Переписати задачу 5 на Switch
-
+// switch (daysUntilDeadline) {
+//   case 0:
+//     console.log("Сьогодні");
+//     break;
+//   case 1:
+//     console.log("Завтра");
+//     break;
+//   case 2:
+//     console.log("Післязавтра");
+//     break;
+//   default:
+//     console.log("Ще не скоро )");
+//     break;
+// }
 // ## Example 7 - Цикл for
+
+// змінна що відповідає за старт циклу
+// умова що відповідає за припинення циклу
+// код який наближає закінчення циклу
+
+// for (let i = 1; i < 10; i += 1) {
+//   console.log(i);
+// }
+
+// let i = 1;
+// while (i < 10) {
+//   console.log(i);
+//   i += 1;
+// }
+// let i = 0;
+// i++; console.log(i) // 0
+// console.log(i) // 1
+// ++i; console.log(i) // 1
+
+// let i = 0;
+// i = i + 1; // 1
+// i += 1;
+// i++;
+
+// i - +1;
+// i--;
+// i = i - 1;
+
+// i = 1 => 1 < 10 => console.log(1) => i + 1
+// i = 2 => 2 < 10 => console.log(2) => i + 1
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// i = 8 => 8 < 10 => console.log(8) => i + 1
+// i = 10 => 10 < 10 ///
 
 // Написати цикл що виводить в консоль числа від меньшого до більшого в діапазоні від min до max
 // але лише ті що кратні 5-ти
@@ -268,6 +431,16 @@ console.log("first");
 // ```js
 // const max = 100;
 // const min = 20;
+
+// for (let i = min; i <= max; i += 1) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = min; i <= max; i += 5) {
+//   console.log(i);
+// }
 // ```
 
 // ## Example 8
@@ -275,3 +448,32 @@ console.log("first");
 // Де компютер загадує своє значення
 // Користувач вводить свій варіант
 // І потім ми бачимо хто переміг
+
+// 1) Дізнатися варіант компютера
+// 2) Дізнатися варіант користувача
+// 3) Перевірити і визначити переможця
+
+// console.log(Math.random()); // min = 0 max 1
+// let computerChoise = "";
+// const number = Math.random();
+// if (number < 0.33) {
+//   computerChoise = "камінь";
+// } else if (number >= 0.33 && number <= 0.66) {
+//   computerChoise = "ножиці";
+// } else {
+//   computerChoise = "папір";
+// }
+
+// let userChoise = prompt("Введіть свій варіант");
+
+// if (userChoise === computerChoise) {
+//   console.log("Нічия");
+// } else if (
+//   (userChoise === "ножиці" && computerChoise === "папір") ||
+//   (userChoise === "камінь" && computerChoise === "ножиці") ||
+//   (userChoise === "папір" && computerChoise === "камінь")
+// ) {
+//   console.log("Виграв юзер");
+// } else {
+//   console.log(`Виграв компютер і він вибрав ${computerChoise}`);
+// }
